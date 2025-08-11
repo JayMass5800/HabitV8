ed4b9f202abdedc778eff8cd0adb8675087faf4f
JayMass5800 <james.maskrey@gmail.com>
2025-08-11 11:06:04 -0700

    []

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,11 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 // Load keystore properties
 val keystorePropertiesFile = rootProject.file("key.properties")
-val keystoreProperties = java.util.Properties()
+val keystoreProperties = Properties()
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 plugins {
@@ -44,7 +47,7 @@ android {
     }
 
     signingConfigs {
-        release {
+        create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String?
             keyPassword = keystoreProperties["keyPassword"] as String?
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
