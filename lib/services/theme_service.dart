e7ef7ea5515071ea8348fcd13ee1c08ed2e1ef0d
JayMass5800 <james.maskrey@gmail.com>
2025-09-07 14:19:32 -0700

    []

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ThemeService {
   static const String _themeModeKey = 'theme_mode';
