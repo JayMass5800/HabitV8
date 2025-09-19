9aa2f76e029e42afb5d3d9298bdc7f50a0cf78f7
JayMass5800 <james.maskrey@gmail.com>
2025-09-19 14:22:17 -0700

    []

--- android/app/src/debug/kotlin/com/habittracker/habitv8/debug/HabitCompactWidgetProvider.kt
+++ android/app/src/debug/kotlin/com/habittracker/habitv8/debug/HabitCompactWidgetProvider.kt
@@ -5,19 +5,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.habittracker.habitv8.HabitCompactWidgetProvider as MainHabitCompactWidgetProvider
 
-/**
- * Debug wrapper for HabitCompactWidgetProvider
- * This class delegates all calls to the main implementation in the base package
- */
 class HabitCompactWidgetProvider : MainHabitCompactWidgetProvider() {
-    
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray,
         widgetData: SharedPreferences
     ) {
-        // Delegate to parent implementation
         super.onUpdate(context, appWidgetManager, appWidgetIds, widgetData)
     }
 }
--- android/app/src/debug/kotlin/com/habittracker/habitv8/debug/HabitTimelineWidgetProvider.kt
+++ android/app/src/debug/kotlin/com/habittracker/habitv8/debug/HabitTimelineWidgetProvider.kt
@@ -5,19 +5,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.habittracker.habitv8.HabitTimelineWidgetProvider as MainHabitTimelineWidgetProvider
 
-/**
- * Debug wrapper for HabitTimelineWidgetProvider
- * This class delegates all calls to the main implementation in the base package
- */
 class HabitTimelineWidgetProvider : MainHabitTimelineWidgetProvider() {
-    
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray,
         widgetData: SharedPreferences
     ) {
-        // Delegate to parent implementation
         super.onUpdate(context, appWidgetManager, appWidgetIds, widgetData)
     }
 }
